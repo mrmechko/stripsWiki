8e903ecea7db2f5dae140cf3e0a4db885fd318cd
Rik <mrmechko@users.noreply.github.com>
2015-08-07 12:40:52 -0400

    Update Paths.scala

--- wiki/jvm/src/main/scala/wiki/Paths.scala
+++ wiki/jvm/src/main/scala/wiki/Paths.scala
@@ -1,6 +1,7 @@
 package wiki
 
 object Paths {
+  //should have fallbacks for this somewhere
   val wikiBase = sys.env("tripsWikiBase")//"/Users/mechko/projects/co/TripsDiscussion/wiki"
   val tripsXMLBase = sys.env("tripsXMLBase")//"/Users/mechko/nlpTools/flaming-tyrion/lexicon/data/"
 }
